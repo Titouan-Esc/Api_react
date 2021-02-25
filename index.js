34dfa2c303abfaf4812b886ab89b8c1ed133c6dd
Mes Morts <titouanescorneboueu@gmail.com>
2021-02-25 18:29:55 +0100

    Ajout de get et post

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ const posts = [
         contenu : "Ceci est un contenu de mon troisieme post"
     },
 ]
+// ! Si cette action n'est pas faite le server ne marche pas donc il est très important de le mettre
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
 
 
 app.get("/", (req, res) => {
@@ -33,6 +37,9 @@ app.get("/api/v1/posts", (req,res) => {
     res.send(posts)
 })
 
+
+
+// ! get sert à voir ce que j'ai dans mon array
 app.get("/api/v1/posts/:id", (req,res) => {
     const post = posts.find((e) => {
         return e.id === parseInt(req.params.id)
@@ -43,7 +50,29 @@ app.get("/api/v1/posts/:id", (req,res) => {
     res.send(post);
 });
 
-// ! Si cette action n'est pas faite le server ne marche pas donc il est très important de le mettre
-app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
-});
+
+// ! post va ensuite servir à créer du contenu
+app.post("/api/v1/posts", (req,res) => {
+    const schema = Joi.object({
+        titre: Joi.string().min(2).required(),
+        contenu: Joi.string().min(10).required()
+    });
+
+    const result = schema.validate(req.body);
+    console.log(result.error)
+
+    if(result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }else{
+        const post = {
+            id: posts.length + 1,
+            titre: req.body.titre,
+            contenu: req.body.contenu 
+    };
+    posts.push(post)
+    res.send(post)
+    }
+})
+
+// ! put lui sert à update notre post c'est assez simple car il suffit juste de reprendre les choses que nous avons écrite au dessus
